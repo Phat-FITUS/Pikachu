5372427f697ae19c4ea495cc415858d9c6793a4b
HTam <truonghahuytam1@gmail.com>
2023-03-22 08:42:06 +0700

    add Colors library

---
+++ Colors.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef COLOR_H
+#define COLOR_H
+
+struct Colors {
+	int Blue = 1;
+	int Green = 2;
+	int Aqua = 3;
+	int Red = 4;
+	int Purple = 5;
+	int Yellow = 6;
+	int White = 7;
+	int Gray = 8;
+	int LightBlue = 9;
+	int Black = 0;
+	int LightGreen = 10;
+	int LightAqua = 11;
+	int LightRed = 12;
+	int LightPurple = 13;
+	int LightYellow = 14;
+	int BrightWhite = 15;
+};
+
+#endif#pragma once
