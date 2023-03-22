cca02f66aca49987784178a6fc4b394b7975446e
Yukirelia <ltpcoder2k4@gmail.com>
2023-03-23 00:28:35 +0700

    Beautify board code, add new Draw object and set up the game object

--- Pikachu.cpp
+++ Pikachu.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "Game.h"
+
 using namespace std;
 
 int main()
 {
     cout << "Ditme BHT\n";
+    Board game(4, 4);
+    game.fill('A');
+    game.display(2, 2);
+
     return 0;
 }
