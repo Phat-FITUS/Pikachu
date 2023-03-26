a4bf7a18dd651b2c44638a533c9bd4ba5a2569c3
Yukirelia <ltpcoder2k4@gmail.com>
2023-03-27 01:39:05 +0700

    Fix Tam's bug and finish check valid board game

--- Board.h
+++ Board.h
@@ -9,16 +9,18 @@ private:
 	Object draw;
 	const int CELL_WIDTH = 9;
 	const int CELL_HEIGHT = 5;
+	static const int MAX_SIZE = 8;
 	bool selection = true;
 
 	void allocate(); 
 	void deallocate();
+	static bool checkValidSize(int width, int height);
 public:
 	Board();
 	Board(int, int);
 	~Board();
 	void display(int, int);
 	void fill(char);
-	void changeSize(int, int);
+	bool changeSize(int, int);
 };
 
